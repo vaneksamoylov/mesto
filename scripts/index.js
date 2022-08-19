bb08947a869d5c382ae53eabaee67c557b9c3cd0
Ivan Samoilov <venek.samoylov@yandex.ru>
2022-08-19 16:31:57 +0300

    fix mobile, rewrite js functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,54 @@
-console.log('Include js');
-
 let editButton = document.querySelector('.profile__edit-btn');
 let closeButton = document.querySelector('.popup__close-btn');
 let popupEditProfile = document.querySelector('.popup_edit-profile')
-
-editButton.addEventListener('click', function () {
-  console.log('click open')
-  popupEditProfile.classList.add('popup_opened');
-});
-
-closeButton.addEventListener('click', function () {
-  popupEditProfile.classList.remove('popup_opened');
-})
-
-
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input-text-username'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__input-text-job'); // Воспользуйтесь инструментом .querySelector()
+let nameInput = document.querySelector('.popup__input-text_type_username'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__input-text_type_job'); // Воспользуйтесь инструментом .querySelector()
 
 let profileUsername = document.querySelector('.profile__username');
 let profileJob = document.querySelector('.profile__job');
 
+
+function openPopup (el) {
+  el.classList.add('popup_opened');
+};
+
+function closePopup (el) {
+  el.classList.remove('popup_opened');
+};
+
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                          // Так мы можем определить свою логику отправки.
-                          // О том, как это делать, расскажем позже.
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                        // Так мы можем определить свою логику отправки.
+                        // О том, как это делать, расскажем позже.
 
-    // Получите значение полей jobInput и nameInput из свойства value
-    nameInputValue = nameInput.value;
-    jobInputValue = jobInput.value;
-    // Выберите элементы, куда должны быть вставлены значения полей
+  // Получите значение полей jobInput и nameInput из свойства value
+  nameInputValue = nameInput.value;
+  jobInputValue = jobInput.value;
+  // Выберите элементы, куда должны быть вставлены значения полей
 
-    // Вставьте новые значения с помощью textContent
-    profileUsername.textContent = nameInputValue;
-    profileJob.textContent = jobInputValue;
-    popupEditProfile.classList.remove('popup_opened');
+  // Вставьте новые значения с помощью textContent
+  profileUsername.textContent = nameInputValue;
+  profileJob.textContent = jobInputValue;
+  closePopup(popupEditProfile);
 }
+
+editButton.addEventListener('click', function () {
+  openPopup(popupEditProfile);
+  username = profileUsername.innerHTML;
+  job = profileJob.innerHTML;
+  
+  nameInput.value = username;
+  jobInput.value = job;
+});
+
+closeButton.addEventListener('click', function () {
+  closePopup(popupEditProfile);
+});
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
