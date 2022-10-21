edd862fe9c6a67c327a144251668308f49c68ec8
Ivan Samoilov <venek.samoylov@yandex.ru>
2022-10-21 16:47:41 +0300

    add validation

--- scripts/index.js
+++ scripts/index.js
@@ -14,10 +14,19 @@ const profileJob = document.querySelector(".profile__job");
 
 function openPopup(popup) {
   popup.classList.add("popup_opened");
+  document.addEventListener('keydown', closeByEsc);
 }
 
 function closePopup(popup) {
   popup.classList.remove("popup_opened");
+  document.removeEventListener('keydown', closeByEsc);
+}
+
+function closeByEsc(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened');
+    closePopup(openedPopup); 
+  }
 }
 
 function formUserProfileSubmitHandler(evt) {
